2969171b1d5e390534bb912bc965d99702e0b84d
Davi Profeta <davihpg2008@gmail.com>
2026-07-09 09:33:16 -0300

    Add files via upload

---
+++ src/assets/js/feed.js
@@ -0,0 +1,22 @@
+
+// Mostra o texto que estava escondido
+document.querySelectorAll('.description-text').forEach((textElement) => {
+const fullText = textElement.innerText;
+const limit = 150; // Limite de letras
+
+if (fullText.length > limit) {
+    const part1 = fullText.substring(0, limit);
+    const part2 = fullText.substring(limit);
+
+    textElement.innerHTML = `
+      ${part1}<span class="dots">... </span><span class="view-more-btn">ver mais</span><span class="more-text" style="display: none;">${part2}</span>
+    `;
+
+    const btn = textElement.querySelector('.view-more-btn');
+    btn.addEventListener('click', () => {
+      textElement.querySelector('.more-text').style.display = "inline";
+      textElement.querySelector('.dots').style.display = "none";
+      btn.style.display = "none";
+    });
+  }
+});
