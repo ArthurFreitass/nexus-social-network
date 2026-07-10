8d5aad91a7d7475e11c44b0cc96c4df6daff7242
Davi Profeta <davihpg2008@gmail.com>
2026-07-10 11:11:05 -0300

    Update feed.js

--- src/assets/js/feed.js
+++ src/assets/js/feed.js
@@ -21,6 +21,7 @@ if (fullText.length > limit) {
   }
 });
 
+
 // Novo Post
 const btnNewPost = document.getElementById("newPost")
 btnNewPost.addEventListener("click", addPost)
@@ -29,5 +30,58 @@ function addPost(){
     const postNew = document.getElementById("modalPost")
     postNew.style.transition = "all ease 1s"
 
+    postNew.addEventListener('click', (event) => {
+    const rect = postNew.getBoundingClientRect();
+    
+    const clicouFora = (
+        event.clientX < rect.left ||
+        event.clientX > rect.right ||
+        event.clientY < rect.top ||
+        event.clientY > rect.bottom
+    );
+
+    // Se o clique foi fora, fecha o modal
+    if (clicouFora) {
+        postNew.close();
+    }
+});
+
     postNew.showModal();
 }
+
+// Colocar foto e musica
+const labelMusic = document.getElementById("labelMusic");
+labelMusic.addEventListener("dragenter", onEnterMusic);
+labelMusic.addEventListener("drop", onLeaveMusic);
+labelMusic.addEventListener("dragover", onEnterMusic);
+labelMusic.addEventListener("dragleave", onLeaveMusic);
+
+function onEnterMusic(){
+    labelMusic.classList.add("active");
+}
+function onLeaveMusic(){
+    labelMusic.classList.remove("active");
+}
+
+const labelPhoto = document.getElementById("labelPhoto");
+labelPhoto.addEventListener("dragenter", onEnterPhoto);
+labelPhoto.addEventListener("drop", onLeavePhoto);
+labelPhoto.addEventListener("dragover", onEnterPhoto);
+labelPhoto.addEventListener("dragleave", onLeavePhoto);
+
+function onEnterPhoto(){
+    labelPhoto.classList.add("active");
+}
+function onLeavePhoto(){
+    labelPhoto.classList.remove("active");
+}
+
+//Imagem 
+/*const input = document.getElementById("upPhoto");
+input.addEventListener("change", event => {
+  if(input.files.length > 0)
+
+    //console.log(input.files);
+    const type = input.files[0].type
+    console.log(type);
+})*/
